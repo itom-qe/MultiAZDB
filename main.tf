95bdc57a7c1f928fd6e03a5fdaf2962d9ce2240b
itom-qe <79703359+itom-qe@users.noreply.github.com>
2021-05-17 21:04:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,10 +64,39 @@ resource "azurerm_sql_server" "server1" {
 }
 
 resource "azurerm_sql_firewall_rule" "fw1" {
-  name                = "${var.db_name}-fwrules1"
+  name                = "${var.db1_name}-fwrules1"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   server_name         = "${azurerm_sql_server.server1.name}"
   start_ip_address    = "${var.start_ip_address}"
   end_ip_address      = "${var.end_ip_address}"
 }
 
+
+resource "azurerm_sql_database" "db2" {
+  name                             = "${var.db2_name}"
+  resource_group_name              = "${azurerm_resource_group.rg.name}"
+  location                         = "${var.region}"
+  edition                          = "${var.db_edition}"
+  collation                        = "${var.collation}"
+  server_name                      = "${azurerm_sql_server.server2.name}"
+  create_mode                      = "Default"
+  requested_service_objective_name = "${var.service_objective_name}"
+  tags                             = "${var.tags}"
+}
+resource "azurerm_sql_server" "server1" {
+  name                         = "${var.db2_name}-sqlsvr2"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  location                     = "${var.region}"
+  version                      = "${var.server_version}"
+  administrator_login          = "${var.sql_admin_username}"
+  administrator_login_password = "${var.sql_password}"
+  tags                         = "${var.tags}"
+}
+
+resource "azurerm_sql_firewall_rule" "fw1" {
+  name                = "${var.db2_name}-fwrules2"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  server_name         = "${azurerm_sql_server.server2.name}"
+  start_ip_address    = "${var.start_ip_address}"
+  end_ip_address      = "${var.end_ip_address}"
+}
